9ab9519e1fa93f61ce82d37d14856d09d5ff0ec4
Raunak Pathak <raunakpathak2703@gmail.com>
2025-12-11 03:31:27 +0530

    adding_rag

--- Agents/paracetamol_agent.py
+++ Agents/paracetamol_agent.py
@@ -1,20 +1,22 @@
 from utils import call_llm,call_llm_with_history,safe_eval
+from rag_pipeline import rag_answer
 
 MODEL = 'google/gemini-2.5-flash-lite'
 
 
 def paracetamol_agent(message):
     print('paracetamol_agent is running')
-    system_message = '''You are a helpful assistant that answers questions only related to paracetamol and nothing else.
-                        If any other drug is mentioned then completly ignore its mention and do not talk about it.
-                        Return results in Dict format where the keys are different medicine names
-                        Return ONLY a Python dict.
-                        Do NOT wrap the response in backticks.
-                        Do NOT use JSON.
-                        Do NOT add explanations.
-                        Example : user message : what is paracetamol. what is insulin
-                                  assistant message : {'paracetamol' : 'details about paracetamol',
-                                                        'insulin' : 'details about insulin'}'''
-    raw_paractamol_output = call_llm(MODEL, system_message, message)
-    paracetamol = safe_eval(raw_paractamol_output)
-    return paracetamol['paracetamol']
+    # system_message = '''You are a helpful assistant that answers questions only related to paracetamol and nothing else.
+    #                     If any other drug is mentioned then completly ignore its mention and do not talk about it.
+    #                     Return results in Dict format where the keys are different medicine names
+    #                     Return ONLY a Python dict.
+    #                     Do NOT wrap the response in backticks.
+    #                     Do NOT use JSON.
+    #                     Do NOT add explanations.
+    #                     Example : user message : what is paracetamol. what is insulin
+    #                               assistant message : {'paracetamol' : 'details about paracetamol',
+    #                                                     'insulin' : 'details about insulin'}'''
+    raw_paractamol_output = rag_answer(message)
+    # paracetamol = safe_eval(raw_paractamol_output)
+    # return paracetamol['paracetamol']
+    return raw_paractamol_output
