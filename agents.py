58f5e7b1d2cf084a0671c3350ae15585d088bf5c
Raunak Pathak <raunakpathak2703@gmail.com>
2025-12-10 21:41:41 +0530

    Initial commit

--- agents.py
+++ agents.py
@@ -11,13 +11,15 @@ openrouter = OpenAI(base_url=openrouter_url, api_key=openrouter_api_key)
 MODEL = 'google/gemini-2.5-flash-lite'
 
 def cat_agent(message):
-    system_message = '''You are a helpful assistant that catergorizes the given message into three catergories
+    system_message = '''You are a helpful assistant that catergorizes the given message into two catergories.
                         Category 1 : General Query
                         Category 2 : Drug Information
+                        If the message contains any drug or medicine name then put it into Category 2 : Drug Information
     '''
     messages = [{"role": "system", "content": system_message}, {"role": "user", "content": message}]
     response = openrouter.chat.completions.create(model=MODEL, messages=messages)
     category = response.choices[0].message.content
+    print(category)
     return category
 
 def reply_normal_agent(message,history):
@@ -26,31 +28,30 @@ def reply_normal_agent(message,history):
     response = openrouter.chat.completions.create(model=MODEL, messages=messages)
     return response.choices[0].message.content
 
-def paracetamol_agent(message,history):
+def paracetamol_agent(message):
     system_message = 'You are a helpful assistant that answers questions only related to paracetamol and nothing else.'
-    messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    messages = [{"role": "system", "content": system_message},{"role": "user", "content": message}]
     response = openrouter.chat.completions.create(model=MODEL, messages=messages)
     return response.choices[0].message.content
 
-def insulin_agent(message,history):
+def insulin_agent(message):
     system_message = 'You are a helpful assistant that answers questions only related to insulin and nothing else.'
-    messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    messages = [{"role": "system", "content": system_message},{"role": "user", "content": message}]
     response = openrouter.chat.completions.create(model=MODEL, messages=messages)
     return response.choices[0].message.content
-    
 
-def drug_id(message,history):
+def drug_id(message):
     system_message = '''You are a helpful assistant that picks out all the drug names in a query and gives a list of the drug names present in the query.
                         Return only python list
                         Fix the typos as well if you find any'''
-    messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    messages = [{"role": "system", "content": system_message},{"role": "user", "content": message}]
     response = openrouter.chat.completions.create(model=MODEL, messages=messages)
     drugs = response.choices[0].message.content
-    # print(drugs.lower())
+    print(drugs.lower())
     if 'paracetamol' in drugs.lower() :
-        return paracetamol_agent(message,history)
+        return paracetamol_agent(message)
     if 'insulin' in drugs.lower() :
-        return insulin_agent(message,history)    
+        return insulin_agent(message)    
     else :
         return 'invalid drug. I only answer questions related to paracetamol and insulin'
 
--- orchestrator.py
+++ orchestrator.py
@@ -2,9 +2,9 @@ from agents import cat_agent, reply_normal_agent, drug_id
 
 def orch(message,history) :
     catergory = cat_agent(message)
-    if 'Category 1 : General Query' in catergory:
+    if 'General Query' in catergory:
         return reply_normal_agent(message,history)
-    elif 'Category 2 : Drug Information' in catergory:
-        return drug_id(message,history)
+    elif 'Drug Information' in catergory:
+        return drug_id(message)
     else :
         return "Sorry, I could not categorize your question. Please try rephrasing it."
